52cd803f232ec5b83ad32b843f9beba237c3bf2d
umarrashid <umarrashid@hotmail.dk>
2024-04-15 15:26:22 +0200

    Prepared to host on server

---
+++ Agora-app/src/routes/error.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export function load({ error }) {
+		return {
+			status: error.status,
+			error: error.message,
+		};
+	}
+</script>
+
+<script>
+	export let status;
+	export let error;
+</script>
+
+<h1>{status}</h1>
+<p>{error}</p>
+<p>Sorry, the page you are looking for could not be found.</p>
