c79f4d8a51c0c070bd8a13366d2a8b4d4093bb90
umarrashid <umarrashid@hotmail.dk>
2024-04-22 14:34:04 +0200

    Comment cors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,18 @@ const app = express();
 
 app.use(express.json());
 
+const allowedOrigins = ['https://agora.servernux.com', 'http://130.225.39.205'];
+
 app.use(cors({
-	origin: 'https://agora.servernux.com',
+	origin: function(origin, callback){
+		// allow requests with no origin (like mobile apps or curl requests)
+		if(!origin) return callback(null, true);
+		if(allowedOrigins.indexOf(origin) === -1){
+			var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	},
 	credentials: true
 }));
 
