a37ab64b5974f1cbef9d927f44121f8397b95de1
umarrashid <umarrashid@hotmail.dk>
2024-04-23 13:53:29 +0200

    Comment out cors

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,22 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ['https://agora.servernux.com', 'http://130.225.39.205'];
-
-app.use(cors({
-	origin: function(origin, callback){
-		// allow requests with no origin (like mobile apps or curl requests)
-		if(!origin) return callback(null, true);
-		if(allowedOrigins.indexOf(origin) === -1){
-			var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	},
-	credentials: true
-}));
-
+// <
+// const allowedOrigins = ['https://agora.servernux.com', 'http://130.225.39.205'];
+//
+// app.use(cors({
+// 	origin: function(origin, callback){
+// 		// allow requests with no originike mobile apps or curl requests)
+// 		if(!origin) return callback(null, true);
+// 		if(allowedOrigins.indexOf(origin) === -1){
+// 			var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+// 			return callback(new Error(msg), false);
+// 		}
+// 		return callback(null, true);
+// 	},
+// 	credentials: true
+// }));
+>
 let otpStore = {};
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/agora.servernux.com/privkey.pem', 'utf8');
