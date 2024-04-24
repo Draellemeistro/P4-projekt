b10d08a6a5eab93d2bb48ddd6b602e373aed2288
umarrashid <umarrashid@hotmail.dk>
2024-04-24 17:24:04 +0200

    Fixed ballotbox sql query

--- backend/server.js
+++ backend/server.js
@@ -202,7 +202,7 @@ app.post('/insert-ballot', (req, res) => {
 	const enc_ballot = req.body.ballot;
 	const enc_voter = req.body.voter;
 	const pubKeyECDH = req.body.pubKeyECDH
-	const query = 'INSERT INTO Agora.encrypted_ballot_box (enc_ballot, enc_voter, pubKeyECDH) VALUES (?, ?, ?)';
+	const query = 'INSERT INTO Agora.ballotbox (ballotbox.encr_ballot, encr_voter_id, ECDH_pub_key) VALUES (?, ?, ?)';
 	connection.query(query, [enc_ballot, enc_voter, pubKeyECDH], (err, results) => {
 		if (err) {
 			console.error(err);
