f6fa575fde371453dd93692dc20a6241900968e3
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-04-29 14:34:50 +0200

    test crypto

--- Agora-app/src/routes/cryptoTests/+page.svelte
+++ Agora-app/src/routes/cryptoTests/+page.svelte
@@ -3,22 +3,15 @@
 	import RSACrypto from '../../utils/encryptionRSA.js';
 	import {
 		initECDH,
-		// eslint-disable-next-line no-unused-vars
-		getPublicKey,
-		computeSharedSecret,
-		requestServerPublicKeyECDH,
-		encryptMessageECDH,
-		// eslint-disable-next-line no-unused-vars
-		performECDHAndEncryptBallot,
-		// eslint-disable-next-line no-unused-vars
-		performTestECDHAndEncryptBallot
 	} from '../../utils/encryptionECDH.js';
 	// eslint-disable-next-line no-unused-vars
 	import RSABlindSig from '../../utils/blindSignatureRSA.js';
 
 	let rsaPublicKey;
 	let ecdhKeys;
+	// eslint-disable-next-line no-unused-vars
 	let sharedSecret;
+	// eslint-disable-next-line no-unused-vars
 	let encryptedMessage;
 
 	onMount(async () => {
@@ -26,14 +19,7 @@
 		ecdhKeys = initECDH();
 	});
 
-	async function testECDH() {
-		const serverPublicKeyBase64 = await requestServerPublicKeyECDH();
-		sharedSecret = computeSharedSecret(ecdhKeys.client, serverPublicKeyBase64);
-	}
 
-	function testEncryption() {
-		encryptedMessage = encryptMessageECDH('Test message', sharedSecret);
-	}
 </script>
 
 <div>
@@ -46,16 +32,4 @@
 	<h2>Initiate ECDH</h2>
 	<button on:click={initECDH}>Initiate ECDH</button>
 	<p>ECDH Keys: {ecdhKeys}</p>
-</div>
-
-<div>
-	<h2>Test ECDH</h2>
-	<button on:click={testECDH}>Test ECDH</button>
-	<p>Shared Secret: {sharedSecret}</p>
-</div>
-
-<div>
-	<h2>Test Encryption</h2>
-	<button on:click={testEncryption}>Test Encryption</button>
-	<p>Encrypted Message: {encryptedMessage}</p>
 </div>
