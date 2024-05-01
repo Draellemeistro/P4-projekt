41a914ebf91a60231fa3133c28669d3068af106e
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-01 11:18:31 +0200

    test crypto

--- backend/utils/RSACryptoUtils.js
+++ backend/utils/RSACryptoUtils.js
@@ -40,7 +40,7 @@ const serverRSACrypto = {
 	},
 	encryptWithPubRSA: function encryptWithPublicKey(message, publicKey) {
 	const buffer = Buffer.from(message);
-	const pubKeyBuffer = Buffer.from(publicKey, 'base64');
+	const pubKeyBuffer = Buffer.from(publicKey);
 	const encrypted = crypto.publicEncrypt({
 			key: pubKeyBuffer,
 			padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
