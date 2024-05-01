e2af04e2391d19256e145f21e718e04359f7c464
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-01 21:03:22 +0200

    ECDH (WIP):fixing ECDH server keys....

--- backend/server.js
+++ backend/server.js
@@ -200,9 +200,7 @@ app.post('/fetch-candidates', (req, res) => {
 
 app.post('/request-public-ecdh-key', (req, res) => {
 	console.log('Accessed /request-public-ecdh-key endpoint');
-	const keyBuffer = Buffer.from(serverPublicKeyECDH, 'base64');
-	const jwk = JWK.asKey(keyBuffer, { alg: 'ECDH', use: 'enc' })
-	res.json(jwk);
+	res.json(serverPublicKeyECDH);
 	console.log('ECDH Public Key sent');
 }	);
 app.post('/rsa-public-key', (req, res) => {
