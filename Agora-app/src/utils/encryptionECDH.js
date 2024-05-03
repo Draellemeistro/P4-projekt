a8cb390f634486055bc0da3f1f9c929c1c415dc8
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-03 12:22:54 +0200

    ECDH WIP: currently working on getting server to decrypt message
    WORKS: (initClientECDH, requestServerECDH, tempSendECDHKey, deriveSecret, verifySharedSecretTest, encryptECDH),
    
    WIP: (decryptECDH) (lots of clean up and refactoring)

--- Agora-app/src/utils/encryptionECDH.js
+++ Agora-app/src/utils/encryptionECDH.js
@@ -473,17 +473,14 @@ const ECDHCrypto ={
 // Function to perform ECDH key exchange, encrypt ballot, and send it to server
 	// eslint-disable-next-line no-undef
 	SendEncryptedMsgTest: async function performECDHAndEncryptBallot(plainTextMessage,encryptedMessage, clientPublicKey, ivValue) {
+		let encryptedMessageBase64 = this.convertArrBuffToBase64(encryptedMessage);
 		let msgForServer = JSON.stringify({
 			plainTextMessage: plainTextMessage,
-			encryptedMessage: encryptedMessage,
+			encryptedMessage: encryptedMessageBase64,
 			clientPublicKey: clientPublicKey,
 			IvValue: ivValue
 		});
 		//console.log('msgForServer: ', msgForServer);
-		console.log('function received this encrypted message: ', encryptedMessage);
-		console.log('stringify of encrypted message: ', JSON.stringify(encryptedMessage));
-		console.log('sending this encrypted message: ', msgForServer.encryptedMessage);
-
 		const response = await fetch('https://192.168.0.113:3030/decrypt-ECDH-message-Test', {
 				method: 'POST',
 				headers: {
