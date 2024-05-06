25178f147b6f45b65336dbe6b93c4538447a15ad
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-06 12:10:34 +0200

    Clean up & refactoring

--- Agora-app/src/utils/apiServiceDev.js
+++ Agora-app/src/utils/apiServiceDev.js
@@ -4,11 +4,18 @@ const serverPort = '3030';
 //import crypto from 'crypto';
 
 export const DecryptTestRSA = async (plainTextMessage, encryptedMessage) => {
-	return await fetch(`https://${serverIP}:${serverPort}/decrypt-RSA-message-Test'`, {
+	let response = await fetch(`https://${serverIP}:${serverPort}/decrypt-RSA-message-Test`, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({ plainTextMessage: plainTextMessage, encryptedMessage: encryptedMessage }),
+		body: JSON.stringify({ plainTextMessage, encryptedMessage }),
 	});
-};
+	if (response.ok) {
+		return response.json();
+	}
+	else {
+		console.error('Error in DecryptTestRSA: ', response.status);
+		return response.status;
+	}
+}
