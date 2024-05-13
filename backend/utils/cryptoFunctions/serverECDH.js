204d772c07ba5b82612a6e8fc081bc8f40cac0c4
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 17:22:26 +0200

    renamed ServerDigitalSignatures.js since server couldnt locate it

--- backend/utils/cryptoFunctions/serverECDH.js
+++ backend/utils/cryptoFunctions/serverECDH.js
@@ -30,7 +30,7 @@ const serverECDH = {
 		const serverPubKeyJWK = JSON.parse(serverPubKeyJWKString);
 		const serverPrivKeyJWK = JSON.parse(serverPrivKeyJWKString);
 		this.pubKey = this.keyImportTemplateECDH(serverPubKeyJWK, true);
-		this.privKey = this.keyImportTemplateECDH(serverPrivKeyJWK);
+		this.privKey = this.keyImportTemplateECDH(serverPrivKeyJWK, false);
 	},
 	importClientKey: async function importClientKey(clientKeyString) {
 		this.clientPubKey = await this.keyImportTemplateECDH(clientKeyString, true);
