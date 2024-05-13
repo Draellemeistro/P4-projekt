d38ecdb7c916a5790f04a46811db23c3cea633c8
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 20:13:09 +0200

    Fixed import of server private ECDH key

--- Agora-app/src/routes/cryptoTests/+page.svelte
+++ Agora-app/src/routes/cryptoTests/+page.svelte
@@ -2,17 +2,28 @@
 	import { onMount } from 'svelte';
 	import { packageAndExchangeKeys } from './cryptoTests.js';
 	import cryptoUtils from '../../utils/cryptoUtils.js';
-
+	let keyStatus = 'have not checked yet';
+	async function checkKeyStatus() {
+		if (cryptoUtils.ECDH.pubKey) {
+			if(cryptoUtils.ECDH.pubKey instanceof CryptoKey){
+				console.log('Public key:', cryptoUtils.ECDH.pubKey);
+				keyStatus = 'Public key exists';
+			}
+		} else {
+			console.log('No public key');
+			keyStatus = 'No public key';
+		}
+	}
 
 	onMount(async () => {
-		//await cryptoUtils.genBothKeys();
+		await cryptoUtils.genBothKeys();
 	});
-
 
 </script>
 <button on:click={packageAndExchangeKeys}>exchange keys</button>
 <button on:click={cryptoUtils.genBothKeys()}>generate new keys</button>
-
+<button on:click={checkKeyStatus}>Check key status</button>
+<p>keyStatus message: {keyStatus}</p>
 
 <input type="button" onclick="location.href='/cryptoTests/testingECDH';" value="go to ecdh" /><input type="button" onclick="location.href='/cryptoTests/testingRSA';" value="Go to rsa" />
-<input type="button" onclick="location.href='/cryptoTests/testingDigSig';" value="Go to voting page" /><input type="button" onclick="location.href='/cryptoTests/dummyVote';" value="Go to testing main page" />
+<input type="button" onclick="location.href='/cryptoTests/testingDigSig';" value="Go to digSig" /><input type="button" onclick="location.href='/cryptoTests/dummyVote';" value="Go to dummyvote" />
