f3d65c0dec40849e420dcf2eb420fbde7664cc4f
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 20:38:52 +0200

    Fixed import of server private ECDH key

--- Agora-app/src/routes/cryptoTests/+page.svelte
+++ Agora-app/src/routes/cryptoTests/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
 	import { packageAndExchangeKeys } from './cryptoTests.js';
-
-	import { navigate } from 'svelte-routing';
+	import TestingECDH from './testingECDH/+page.svelte';
+	import TestingRSA from './testingRSA/+page.svelte';
+	import TestingDigSig from './testingDigSig/+page.svelte';
+	import dummyVote from './testingDigSig/+page.svelte';
+	import { Router, Route, navigate } from 'svelte-routing';
 
 	import cryptoUtils from '../../utils/cryptoUtils.js';
 	let keyStatus = 'have not checked yet';
@@ -35,7 +38,12 @@
 		await cryptoUtils.genBothKeys();
 	});
 </script>
-
+<Router>
+	<Route path="/cryptoTests/testingECDH" component={TestingECDH} />
+	<Route path="/cryptoTests/testingRSA" component={TestingRSA} />
+	<Route path="/cryptoTests/testingDigSig" component={TestingDigSig} />
+	<Route path="/cryptoTests/dummyVote" component={dummyVote} />
+</Router>
 
 <button on:click={packageAndExchangeKeys}>exchange keys</button>
 <button on:click={cryptoUtils.genBothKeys()}>generate new keys</button>
