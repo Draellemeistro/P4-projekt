b96036f7475a66ba6fcf76c9d48abe223a3f075e
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 19:14:19 +0200

    Fixed import of server private ECDH key

--- Agora-app/src/routes/cryptoTests/+page.svelte
+++ Agora-app/src/routes/cryptoTests/+page.svelte
@@ -1,30 +1,18 @@
 <script>
 	import { onMount } from 'svelte';
 	import { packageAndExchangeKeys } from './cryptoTests.js';
-	import { Link } from "svelte-routing";
-	//import cryptoUtils from '../../utils/cryptoUtils.js';
+	import cryptoUtils from '../../utils/cryptoUtils.js';
 
 
 	onMount(async () => {
 		//await cryptoUtils.genBothKeys();
 	});
 
+
 </script>
-<button on:click={packageAndExchangeKeys}>Check decrypt</button>
+<button on:click={packageAndExchangeKeys}>exchange keys</button>
+<button on:click={cryptoUtils.genBothKeys()}>generate new keys</button>
 
 
-<Link to="/testingRSA">
-	<button>Go to RSA test</button>
-</Link>
-
-<Link to="/testingECDH">
-	<button>Go to ECDH test</button>
-</Link>
-
-<Link to="/testingDigSig">
-	<button>Go to DigSig test</button>
-</Link>
-
-<Link to="/dummyVote">
-	<button>Go to dummyVote</button>
-</Link>
+<input type="button" onclick="location.href='/cryptoTests/testingECDH';" value="go to ecdh" /><input type="button" onclick="location.href='/cryptoTests/testingRSA';" value="Go to rsa" />
+<input type="button" onclick="location.href='/cryptoTests/testingDigSig';" value="Go to voting page" /><input type="button" onclick="location.href='/cryptoTests/dummyVote';" value="Go to testing main page" />
