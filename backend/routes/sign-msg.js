e5ea25fb95e56901b18397c3c35e2976f1852cdf
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 23:41:24 +0200

    found way to pass key object around using SPA. It's stupid and weird, but hey

--- backend/routes/sign-msg.js
+++ backend/routes/sign-msg.js
@@ -5,10 +5,10 @@ const express = require('express');
 const router = express.Router();
 router.post('/', async (req, res) => {
 	console.log('Accessed /sign-msg endpoint');
-	const body = JSON.parse(req.body);
-	const message = body.message;
-	const signature = body.signature;
+	const message = req.body.message;
+	const signature = req.body.signature;
 	const verified = await serverDigSig.verifyReceivedMessage(message, signature);
+	console.lo
 	res.json({result: verified})
 });
 
