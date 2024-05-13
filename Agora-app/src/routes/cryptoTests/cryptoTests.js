a79c62d2d284edb4ba7693bb623c4e3e03c09973
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-13 22:05:23 +0200

    found way to pass key object around using SPA. It's stupid and weird, but hey

--- Agora-app/src/routes/cryptoTests/cryptoTests.js
+++ Agora-app/src/routes/cryptoTests/cryptoTests.js
@@ -5,22 +5,25 @@ export const packageAndExchangeKeys = async () => {
 	console.log('Accessed packageAndExchangeKeys');
 	const keyRing = await cryptoUtils.packagePublicKeys();
 	const response = await exchangeKeys(keyRing);
+	let serversKeyRing
 	console.log('Response: ', response);
 	if (response.ok) {
 		const data = await response.json();
 		console.log('Data: ', data);
-		if (typeof data === 'string') {
-			const keyRing = JSON.parse(data.keyRing);
-			cryptoUtils.ECDH.saveServerKey(JSON.parse(keyRing.ECDH)).then(r => {
-				console.log('Server ECDH key saved: ', r);
-			});
-			await cryptoUtils.digSig.saveServerKey(JSON.parse(keyRing.DigSig));
-			await cryptoUtils.RSA.saveServerKey(JSON.parse(keyRing.RSA));
-			console.log('Keyring: ', keyRing);
-			console.log('Server keys saved');
-			console.log(keyRing.ECDH);
-			return data;
+		if (typeof data.keyRing === 'string') {
+			 serversKeyRing = JSON.parse(data.keyRing);
+		} else {
+			serversKeyRing = data.keyRing;
 		}
+		cryptoUtils.ECDH.saveServerKey(JSON.parse(serversKeyRing.ECDH)).then(r => {
+			console.log('Server ECDH key saved: ', r);
+		});
+		await cryptoUtils.digSig.saveServerKey(JSON.parse(serversKeyRing.DigSig));
+		await cryptoUtils.RSA.saveServerKey(JSON.parse(serversKeyRing.RSA));
+		console.log('Keyring: ', serversKeyRing);
+		console.log('Server keys saved');
+		console.log(serversKeyRing.ECDH);
+		return serversKeyRing;
 	}
 	else {
 		console.error('Error in ExchangeKeys: ', response.status);
