36350fb23d8898cb0e5a0808af0dd2df4c06f5df
umarrashid <umarrashid@hotmail.dk>
2024-05-15 10:32:00 +0200

    Temporary changed RSA key path to ECDH

--- backend/utils/cryptoFunctions/serverRSA.js
+++ backend/utils/cryptoFunctions/serverRSA.js
@@ -1,6 +1,5 @@
 const crypto = require('crypto');
 const fs = require('fs');
-const { join } = require('path');
 
 
 
@@ -10,12 +9,12 @@ const serverRSA = {
 	privKey: null,
 
 	readPubKeyFromFile: async () => {
-		const serverPubKeyString = fs.readFileSync(join(__dirname, '/serverPublicKeyECDH.json'), 'utf8');
+		const serverPubKeyString = fs.readFileSync('./utils/keys/serverPublicKeyECDH.json', 'utf8');
 		return await serverRSA.keyImportTemplateRSA(serverPubKeyString, true);
 	},
 
 	readPrivKeyFromFile: async () => {
-		const serverPrivKeyString = fs.readFileSync(path.join(__dirname, '/serverPrivateKeyECDH.json'), 'utf8');
+		const serverPrivKeyString = fs.readFileSync('./utils/keys/serverPrivateKeyECDH.json', 'utf8');
 		return await serverRSA.keyImportTemplateRSA(serverPrivKeyString, false);
 	},
 
