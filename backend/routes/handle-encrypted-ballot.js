7d829042d3cdf19534677ca231f281cdcc9acf59
umarrashid <umarrashid@hotmail.dk>
2024-05-15 11:50:17 +0200

    Redo handle-encrypted-ballot.js

--- backend/routes/handle-encrypted-ballot.js
+++ backend/routes/handle-encrypted-ballot.js
@@ -4,86 +4,26 @@ const serverECDHCrypto = require('../utils/cryptoFunctions/serverECDH.js');
 const serverDigSig = require('../utils/cryptoFunctions/ServerDigSig');
 const router = express.Router();
 
-
-
 router.post('/', async (req, res) => {
-	const message = req.body.message;
-	const messageObject = JSON.parse(message);
-	const signature = req.body.signature;
+	try {
+		const { message, signature } = req.body;
+		const { encryptedMessage, ivValue, clientPubKey } = JSON.parse(message);
 
-	// ballot can be stored even before first round of decryption. Could save them for later use?
-	const decryptedMessage = await serverECDHCrypto.handleEncryptedMessage(
-		messageObject.encryptedMessage,
-		messageObject.ivValue,
-		messageObject.clientPubKey//serverECDHCrypto.clientPubKey
-	);
+		const decryptedMessage = await serverECDHCrypto.handleEncryptedMessage(
+			encryptedMessage,
+			ivValue,
+			clientPubKey
+		);
 
-	let voteID;
-	let voteIdEligible = false;
-	let otherInformation;
-	let subMessageObject;
-	console.log('Decrypted message:', decryptedMessage);
-	// TODO do something to utilize the otherInformation object (VoteID, etc.)
-	if (typeof decryptedMessage === 'string') {
-		subMessageObject = JSON.parse(decryptedMessage);
+		const { voteID, otherInformation } = JSON.parse(decryptedMessage);
+
+		// TODO: Add logic to handle voteID and otherInformation
+
+		console.log('Decrypted message:', decryptedMessage);
+	} catch (error) {
+		console.error('Error while handling encrypted ballot:', error);
+		res.status(500).json({ message: 'Internal server error' });
 	}
-	if (typeof subMessageObject.otherInformation === 'string') {
-		otherInformation = JSON.parse(subMessageObject.otherInformation);
-
-	} else {
-		otherInformation = subMessageObject.otherInformation;
-	}
-	voteID = otherInformation.voteID;
-
-	// TODO: Use vote ID to get the correct clientKey to verify the signature.
-	const verify = await serverDigSig.verifyReceivedMessage(signature, message);
-	if (verify) {
-		console.log('Signature is valid');
-		// TODO: compare the voteID with the database to ensure it is eligible to vote.
-		voteIdEligible = await checkVoteID(voteID);
-		if (voteIdEligible) {
-			console.log('Vote ID is eligible');
-			const encBallot = subMessageObject.InnerLayer;
-			const receipt = await serverDigSig.prepareSignatureToSend(encBallot);
-			const responseDB = await storeAcceptedBallot(encBallot, receipt);
-			if (responseDB) {
-				console.log('Ballot stored successfully');
-			}
-			//TODO: save hash of voteID+publicKey to database???
-		} // TODO: check DB stuff
-
-	}
-	const responseSignature = await serverDigSig.prepareSignatureToSend(message);
-	res.json({message: message, signature: responseSignature});
 });
 
-async function checkVoteID(voteId)	{
-	if(voteId)
-		return true;
-}
-
-
-async function storeAcceptedBallot(encBallot, receipt) {
-	const query = 'INSERT INTO Agora.ballotbox (encr_ballot, receipt) VALUES (?, ?)';
-	return connection.query(query, [encBallot, receipt], (err) => {
-			if (err) {
-				console.error(err);
-				console.error('Error inserting data into database');
-			}
-			return true;
-		}
-	);
-}
-
-
 module.exports = router;
-
-//function logObjectEntriesWithType(obj) {
-//	if(typeof obj !== 'object') {
-//		console.log('Invalid object');
-//		return;
-//	}
-//	for (const [key, value] of Object.entries(obj)) {
-//		console.log(`Key: ${key}, Value: ${value}, Type: ${typeof value}`);
-//	}
-//}
