246c14b0873c0d02489012ea7c4a4e697f7d5932
umarrashid <umarrashid@hotmail.dk>
2024-05-15 14:41:11 +0200

    please work i will never be bad again

--- backend/routes/fetch-candidates.js
+++ backend/routes/fetch-candidates.js
@@ -2,16 +2,25 @@ const express = require('express');
 const connection = require('../utils/db.js');
 const { verifyToken } = require('../utils/jwt.js');
 
-
 const router = express.Router();
-router.post('/', verifyToken, (req, res) => {
-	connection.query('SELECT candidate, party FROM Agora.ballot', (err, results) => {
-		if (err) {
-			console.error(err);
-			res.status(500).send('Error fetching candidates from database');
-		} else {
-			res.json(results);
-		}
+router.post('/', (req, res) => {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
+	if (token == null) return res.sendStatus(401); // if there isn't any token
+
+	verifyToken(token, (err, user) => {
+		if (err) return res.sendStatus(403);
+		req.user = user;
+
+		connection.query('SELECT candidate, party FROM Agora.ballot', (err, results) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send('Error fetching candidates from database');
+			} else {
+				res.json(results);
+			}
+		});
 	});
 });
 
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -5,29 +5,27 @@ const SECRET_KEY = '1a2s21j4321231'; // Replace with real key
 // Function to generate a new JWT
 function generateToken(personId, voteId) {
 	const payload = { personId, voteId };
-	return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+
+	token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+	verifyToken(token)
 }
 
 // Middleware function to verify JWT and protect routes
-function verifyToken(req, res, next) {
-	const token = req.headers['authorization'];
-
+function verifyToken(token) {
 	if (!token) {
 		console.log('No token provided');
-		return res.status(403).send({ message: 'No token provided.' });
+		throw new Error('No token provided.');
 	}
 
-	jwt.verify(token, SECRET_KEY, (err, decoded) => {
-		console.log('token:', token);
-		if (err) {
-			console.log('Failed to authenticate token', err);
-			return res.status(500).send({ message: 'Failed to authenticate token.' });
-		}
+	let decoded;
+	try {
+		decoded = jwt.verify(token, SECRET_KEY);
+	} catch (err) {
+		console.log('Failed to authenticate token', err);
+		throw new Error('Failed to authenticate token.');
+	}
 
-		// If token is successfully verified, save decoded info to request for use in other routes
-		req.userId = decoded.id;
-		next();
-	});
+	return decoded;
 }
 
 module.exports = {
