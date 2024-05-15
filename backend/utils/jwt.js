131d7ebe210f2f3bfaa8d3dabb65e8cbfd4f3e4a
umarrashid <umarrashid@hotmail.dk>
2024-05-15 13:54:23 +0200

    minor changes

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = '1a2s21j4321231'; // Replace with real key
 
 // Function to generate a new JWT
-function generateToken(payload) {
+function generateToken(personId, voteId) {
+	const payload = { personId, voteId };
 	return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
 }
 
