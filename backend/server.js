500e0e3fd0dcb0432b8ed5b5e3e1dc184929c653
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2024-05-16 10:21:04 +0200

    Deleted unneeded post routes

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,8 @@ const getEmailRoute = require('./routes/get-email.js');
 const fetchCandidatesRoute = require('./routes/fetch-candidates.js');
 const verify2faRoute = require('./routes/verify-2fa.js');
 const handleEncryptedBallotRoute = require('./routes/handle-encrypted-ballot.js');
-const agreeSharedSecretRoute = require('./routes/agree-shared-secret.js');
 const requestServerKeysRoute = require('./routes/request-server-keys.js');
-const ecdhMsgRoute = require('./routes/ecdh-msg.js');
-const rsaMsgRoute = require('./routes/rsa-msg.js');
 const signMsgRoute = require('./routes/sign-msg.js');
-const verifyMsgRoute = require('./routes/verify-msg.js');
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
@@ -46,12 +42,8 @@ app.use('/get-email', getEmailRoute);
 app.use('/fetch-candidates', fetchCandidatesRoute);
 app.use('/verify-2fa', verify2faRoute);
 app.use('/handle-encrypted-ballot', handleEncryptedBallotRoute);
-app.use('/agree-shared-secret', agreeSharedSecretRoute);
 app.use('/request-server-keys', requestServerKeysRoute);
-app.use('/ecdh-msg', ecdhMsgRoute);
-app.use('/rsa-msg', rsaMsgRoute);
 app.use('/sign-msg', signMsgRoute);
-app.use('/verify-msg', verifyMsgRoute);
 
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/agora.servernux.com/privkey.pem', 'utf8');
